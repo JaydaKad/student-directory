44a6365c2cd9506e96c61b457666c92906276de4
JAYDA KADIR <jydkdr@gmail.com>
2019-05-10 15:36:39 +0100

    Print names only shorter than 12 characters

--- directory.rb
+++ directory.rb
@@ -19,7 +19,7 @@ def print_header
 end
 def print(students)
   students.each_with_index do |student, index|
-    if student[:name][0] == "J"
+    if student[:name][0] == "J" && student[:name].length < 12
       puts "#{index + 1}) #{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
